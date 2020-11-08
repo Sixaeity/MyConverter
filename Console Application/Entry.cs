265a416ee2fd4ad40a51a5cb76d2945f5e7f01de
Sixaeity <72029537+Sixaeity@users.noreply.github.com>
2020-11-08 14:51:36 +0700

    Issue #4; Base console application created.

---
+++ Console Application/Entry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyConverter.ConsoleApp
+{
+    public class Entry
+    {
+        public static void Main(string[] args)
+        {
+            Core core = new Core();
+            core.ProgramLoop();
+        }
+    }
+}
